b848deb05fe7b809a052b50bf885c83125a2d4bd
Robert Nolan <icandothings@gmail.com>
2020-08-20 20:21:55 -0500

    init

---
+++ radio-set.vue
@@ -0,0 +1,24 @@
+<template id="radio-set">
+  <div>
+    <div>{{ labelName }}:</div>
+    <label v-for="(val,key) in options" :key="key">
+      <input
+        type="radio"
+        :name="labelName"
+        :value="val"
+        :checked="val == value"
+        @change="$emit('input','val')"
+      />
+    </label>
+  </div>
+</template>
+<script>
+RadioSetComp = new VueComponent({
+  template: "#radio-set",
+  props: {
+    "label-name": "",
+    value: "",
+    options: ""
+  }
+});
+</script>
